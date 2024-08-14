80f0b83e70a6664c0d54d6eafe3cc43e50574e38
Nik Gospodinov <nik.gospodinov@bt.com>
2024-08-14 11:03:34 +0100

    added a hook for detching exercises

--- constants/url.constants.ts
+++ constants/url.constants.ts
@@ -1,10 +1,11 @@
+import { GetExercisesProps } from "@/services/exercises/get-all-exercises.service";
 import { addQueryParamsToUrl } from "@/utils/url.utils";
 
 export const URL = {
   EXERCISE: {
     base: "https://exercisedb.p.rapidapi.com/exercises/exercise",
 
-    GET_EXERCISES(queryParams: { limit: number; offset: number }): string {
+    GET_EXERCISES(queryParams: GetExercisesProps): string {
       return addQueryParamsToUrl(this.base, queryParams);
     },
 
@@ -24,6 +25,15 @@ export const URL = {
 
     GET_ALL_EQUIPMENT_TYPES(): string {
       return `${this.base}/equipmentList`;
+    },
+
+    // TODO: Be more specific with the target muscle
+    GET_EXERCISE_BY_TARGET_MUSCLE(targetMuscle: string): string {
+      return `${this.base}/target/${targetMuscle}`;
+    },
+
+    GET_ALL_TARGET_MUSCLES(): string {
+      return `${this.base}/targetList`;
     },
   },
 };
---
+++ services/exercises/get-all-exercises.service.ts
@@ -0,0 +1,36 @@
+import { URL } from "@/constants/url.constants";
+import { useQuery } from "@tanstack/react-query";
+
+const EXERCISES_KEY = "exercises";
+
+export type GetExercisesProps = {
+  limit?: number;
+  offset?: number;
+};
+
+const getExercises = async ({ limit = 9999, offset }: GetExercisesProps) => {
+  const url = URL.EXERCISE.GET_EXERCISES({ limit, offset });
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": process.env.RAPID_API_HOST as string,
+      "x-rapidapi-key": process.env.RAPID_API_KEY as string,
+    },
+  });
+
+  if (!response.ok) {
+    console.log(await response.text());
+
+    throw new Error("Failed to fetch exercises");
+  }
+
+  return response.json();
+};
+
+export const useGetExercises = (props: GetExercisesProps) => {
+  return useQuery({
+    queryKey: [EXERCISES_KEY, props],
+    queryFn: () => getExercises(props),
+  });
+};
