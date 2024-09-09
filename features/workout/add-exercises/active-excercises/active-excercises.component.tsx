d80ca1d7b7d642a0cfcf52736267f7a8790bc39e
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-09-09 11:06:49 +0100

    small refactor for better readability of the active exercise component

--- features/workout/add-exercises/active-excercises/active-excercises.component.tsx
+++ features/workout/add-exercises/active-excercises/active-excercises.component.tsx
@@ -1,40 +1,18 @@
 import { VerticalSpacing } from "@/components/ui/layout/vertical-spacing/vertical-spacing.component";
 import { ThemedText } from "@/components/ui/themed-text.component";
 import { useGetExercises } from "@/services/exercises/get-all-exercises.service";
-import { groupBy, capitalize } from "lodash";
-import { FlatList, Pressable, useColorScheme } from "react-native";
-import { Image } from "expo-image";
-import { Flex } from "@/components/ui/layout/flex/flex.component";
-import { spacing } from "@/constants/spacing.constants";
-import { colors } from "@/constants/colors.constants";
-import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { groupBy } from "lodash";
+import { FlatList } from "react-native";
 import { useExerciseFilterStore } from "@/store/exercise-filter-store";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { useState } from "react";
-import { Exercise } from "@/types/api/exercise.types";
+import { ExerciseBox } from "./exercise-box/exercise-box.component";
 
 type Props = {
   search: string;
 };
 
 export const ActiveExercises = ({ search }: Props) => {
-  const theme = useColorScheme() ?? "light";
-
   const insets = useSafeAreaInsets();
-
-  const [selectedExercises, setSelectedExercises] = useState<Exercise[]>([]);
-
-  const onExercisePress = (exercise: Exercise) => {
-    const isSelected = selectedExercises.find(({ id }) => id === exercise.id);
-
-    if (isSelected) {
-      setSelectedExercises(
-        selectedExercises.filter(({ id }) => id !== exercise.id)
-      );
-    } else {
-      setSelectedExercises([...selectedExercises, exercise]);
-    }
-  };
 
   const { bodyPartFilters, equipmentFilters, targetFilters } =
     useExerciseFilterStore();
@@ -61,7 +39,7 @@ export const ActiveExercises = ({ search }: Props) => {
         return (
           <>
             <ThemedText type="small" color="supporting">
-              {item.key}
+              {item.key} {/* this is the letter */}
             </ThemedText>
 
             <VerticalSpacing size={0.5} />
@@ -69,70 +47,7 @@ export const ActiveExercises = ({ search }: Props) => {
             <FlatList
               data={item.value}
               renderItem={({ item: exercise }) => {
-                const isSelected = !!selectedExercises.find(
-                  ({ id }) => id === exercise.id
-                );
-
-                return (
-                  <Pressable
-                    onPress={() => onExercisePress(exercise)}
-                    style={{
-                      backgroundColor: isSelected
-                        ? colors[theme].background
-                        : "transparent",
-                      borderRadius: 16,
-                      padding: spacing[3],
-                    }}
-                  >
-                    <Flex
-                      direction="row"
-                      justify="space-between"
-                      align="center"
-                      gap={spacing[1]}
-                    >
-                      <Flex
-                        direction="row"
-                        gap={spacing[1]}
-                        style={{ flexShrink: 1 }}
-                      >
-                        <Image
-                          source={{ uri: exercise.gifUrl }}
-                          style={{
-                            width: 40,
-                            height: 40,
-                            borderRadius: 100,
-                          }}
-                        />
-
-                        <Flex style={{ flexShrink: 1, gap: spacing[0.5] }}>
-                          <ThemedText>{capitalize(exercise.name)}</ThemedText>
-                          <ThemedText type="small" color="supporting">
-                            {exercise.target}
-                          </ThemedText>
-                        </Flex>
-                      </Flex>
-
-                      <Pressable
-                        style={{
-                          backgroundColor: isSelected
-                            ? colors[theme].border
-                            : colors[theme].background,
-                          width: 32,
-                          height: 32,
-                          alignItems: "center",
-                          justifyContent: "center",
-                          borderRadius: 100,
-                        }}
-                      >
-                        <FontAwesome6
-                          name="question"
-                          size={18}
-                          color={colors[theme].icon}
-                        />
-                      </Pressable>
-                    </Flex>
-                  </Pressable>
-                );
+                return <ExerciseBox exercise={exercise} />;
               }}
             />
 
---
+++ features/workout/add-exercises/active-excercises/exercise-box/exercise-box.component.tsx
@@ -0,0 +1,85 @@
+import { Flex } from "@/components/ui/layout/flex/flex.component";
+import { ThemedText } from "@/components/ui/themed-text.component";
+import { colors } from "@/constants/colors.constants";
+import { spacing } from "@/constants/spacing.constants";
+import { Exercise } from "@/types/api/exercise.types";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { capitalize } from "lodash";
+import { useState } from "react";
+import { Pressable, useColorScheme } from "react-native";
+import { Image } from "expo-image";
+
+type Props = {
+  exercise: Exercise;
+};
+
+export const ExerciseBox = ({ exercise }: Props) => {
+  const theme = useColorScheme() ?? "light";
+
+  const [selectedExercises, setSelectedExercises] = useState<Exercise[]>([]);
+
+  const onExercisePress = (exercise: Exercise) => {
+    const isSelected = selectedExercises.find(({ id }) => id === exercise.id);
+
+    if (isSelected) {
+      setSelectedExercises(
+        selectedExercises.filter(({ id }) => id !== exercise.id)
+      );
+    } else {
+      setSelectedExercises([...selectedExercises, exercise]);
+    }
+  };
+
+  const isSelected = !!selectedExercises.find(({ id }) => id === exercise.id);
+
+  return (
+    <Pressable
+      onPress={() => onExercisePress(exercise)}
+      style={{
+        backgroundColor: isSelected ? colors[theme].background : "transparent",
+        borderRadius: 16,
+        padding: spacing[3],
+      }}
+    >
+      <Flex
+        direction="row"
+        justify="space-between"
+        align="center"
+        gap={spacing[1]}
+      >
+        <Flex direction="row" gap={spacing[1]} style={{ flexShrink: 1 }}>
+          <Image
+            source={{ uri: exercise.gifUrl }}
+            style={{
+              width: 40,
+              height: 40,
+              borderRadius: 100,
+            }}
+          />
+
+          <Flex style={{ flexShrink: 1, gap: spacing[0.5] }}>
+            <ThemedText>{capitalize(exercise.name)}</ThemedText>
+            <ThemedText type="small" color="supporting">
+              {exercise.target}
+            </ThemedText>
+          </Flex>
+        </Flex>
+
+        <Pressable
+          style={{
+            backgroundColor: isSelected
+              ? colors[theme].border
+              : colors[theme].background,
+            width: 32,
+            height: 32,
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 100,
+          }}
+        >
+          <FontAwesome6 name="question" size={18} color={colors[theme].icon} />
+        </Pressable>
+      </Flex>
+    </Pressable>
+  );
+};
