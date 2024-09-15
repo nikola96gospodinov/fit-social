18bc2616a98ac571335a16bc1f1efa54b90c37b7
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-09-15 09:21:52 +0100

    combining body part and target muscle filters

--- app/(tabs)/workout/filters.tsx
+++ app/(tabs)/workout/filters.tsx
@@ -1,17 +1,12 @@
 import { PaddedScrollView } from "@/components/padded-scroll-view/padded-scroll-view.component";
 import { VerticalSpacing } from "@/components/ui/layout/vertical-spacing/vertical-spacing.component";
-import { BodyPartFilters } from "@/features/filters/body-part-filters/body-part-filters.component";
+import { BodyPartAndTargetMuscleFilters } from "@/features/filters/body-part-and-target-muscle-filters/body-part-and-target-muscle-filters.component";
 import { EquipmentFilters } from "@/features/filters/equipment-filters/equipment-filters.component";
-import { TargetMuscleFilters } from "@/features/filters/target-muscle-filters/target-muscle-filters.component";
 
 const Filters = () => {
   return (
     <PaddedScrollView>
-      <BodyPartFilters />
-
-      <VerticalSpacing size={8} />
-
-      <TargetMuscleFilters />
+      <BodyPartAndTargetMuscleFilters />
 
       <VerticalSpacing size={8} />
 
--- constants/workout.constants.ts
+++ constants/workout.constants.ts
@@ -16,9 +16,9 @@ export const targetMuscles = [
   "upper back",
 ] as const;
 
-export type TargetMuscle = (typeof targetMuscles)[number];
-
 export const TARGET_MUSCLE = "target" as const;
+
+export const BODY_PART = "bodyPart" as const;
 
 export const equipment = [
   "assisted",
@@ -55,19 +55,127 @@ export type Equipment = (typeof equipment)[number];
 
 export const EQUIPMENT = "equipment" as const;
 
-export const bodyParts = [
-  "back",
-  "cardio",
-  "chest",
-  "lower arms",
-  "lower legs",
-  "neck",
-  "shoulders",
-  "upper arms",
-  "upper legs",
-  "waist",
+export const primaryBodyPartsAndTargetMuscles = [
+  {
+    type: TARGET_MUSCLE,
+    value: "abs",
+  },
+  {
+    type: BODY_PART,
+    value: "back",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "biceps",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "calves",
+  },
+  {
+    type: BODY_PART,
+    value: "cardio",
+  },
+  {
+    type: BODY_PART,
+    value: "chest",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "forearms",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "glutes",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "hamstrings",
+  },
+  {
+    type: BODY_PART,
+    value: "neck",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "quads",
+  },
+  {
+    type: BODY_PART,
+    value: "shoulders",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "triceps",
+  },
 ] as const;
 
-export type BodyPart = (typeof bodyParts)[number];
+export const secondaryBodyPartsAndTargetMuscles = [
+  {
+    type: TARGET_MUSCLE,
+    value: "abductors",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "adductors",
+  },
+  {
+    type: BODY_PART,
+    value: "lower arms",
+  },
+  {
+    type: BODY_PART,
+    value: "lower legs",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "lats",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "levator scapulae",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "pectorals",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "serratus anterior",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "spine",
+  },
+  {
+    type: TARGET_MUSCLE,
+    value: "traps",
+  },
+  {
+    type: BODY_PART,
+    value: "upper arms",
+  },
+  {
+    type: BODY_PART,
+    value: "upper legs",
+  },
+  {
+    type: BODY_PART,
+    value: "waist",
+  },
+] as const;
 
-export const BODY_PART = "bodyPart" as const;
+export const allBodyPartsAndTargetMuscles = [
+  ...primaryBodyPartsAndTargetMuscles,
+  ...secondaryBodyPartsAndTargetMuscles,
+];
+
+export type TargetMuscle = Extract<
+  (typeof allBodyPartsAndTargetMuscles)[number],
+  { type: typeof TARGET_MUSCLE }
+>["value"];
+
+export type BodyPart = Extract<
+  (typeof allBodyPartsAndTargetMuscles)[number],
+  { type: typeof BODY_PART }
+>["value"];
