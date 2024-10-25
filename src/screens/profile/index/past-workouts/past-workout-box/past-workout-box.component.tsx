4854c26e0a3647012bbc13d80760ce122592c77a
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-10-25 17:04:32 +0100

    feat: more refined design for the past workours box

--- src/screens/profile/index/past-workouts/past-workout-box/past-workout-box.component.tsx
+++ src/screens/profile/index/past-workouts/past-workout-box/past-workout-box.component.tsx
@@ -3,11 +3,15 @@ import { colors } from "@/src/constants/colors.constants";
 import { spacing } from "@/src/constants/spacing.constants";
 import { Tables } from "@/src/types/database.types";
 import { View, StyleSheet, useColorScheme } from "react-native";
-import { formatDistance } from "date-fns";
+import { formatDistance, intervalToDuration } from "date-fns";
 import { Flex } from "@/src/components/ui/layout/flex/flex.component";
 import { ActiveExercise, ExerciseSet } from "@/src/types/workout.types";
 import { VerticalSpacing } from "@/src/components/ui/layout/vertical-spacing/vertical-spacing.component";
 import { capitalize } from "lodash";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { Divider } from "@/src/components/ui/layout/divider/divider.component";
 
 type Props = {
   workout: Tables<"workouts">;
@@ -17,32 +21,73 @@ export const PastWorkoutBox = ({ workout }: Props) => {
   const theme = useColorScheme() ?? "light";
   const exercises = workout.exercises as ActiveExercise[];
 
+  const duration = intervalToDuration({
+    start: new Date(workout.started),
+    end: new Date(workout.ended ?? workout.started),
+  });
+
   return (
     <View
       style={[
         styles.container,
-        { backgroundColor: colors[theme].sectionBackground },
+        { backgroundColor: colors[theme].cardBackground },
       ]}>
       <Flex justify="space-between" direction="row" gap={2} align="center">
-        <ThemedText style={{ fontWeight: "500", fontSize: 18 }}>
-          Chest workout
-        </ThemedText>
+        <View>
+          <ThemedText style={{ fontWeight: "500", fontSize: 18 }}>
+            Chest workout
+          </ThemedText>
 
-        <ThemedText type="extraSmall" color="supporting">
-          {formatDistance(
-            new Date(workout.ended ?? workout.started),
-            new Date(),
-          )}{" "}
-          ago
-        </ThemedText>
+          <VerticalSpacing size={0.5} />
+
+          <ThemedText type="extraSmall" color="supporting">
+            {formatDistance(
+              new Date(workout.ended ?? workout.started),
+              new Date(),
+            )}{" "}
+            ago
+          </ThemedText>
+        </View>
+
+        <FontAwesome name="pencil" size={16} color={colors[theme].icon} />
       </Flex>
 
       <VerticalSpacing size={6} />
 
-      {exercises?.map((exercise) => {
+      <Flex direction="row" gap={4} justify="space-between" align="center">
+        <Flex direction="row" gap={2} align="center">
+          <Ionicons name="timer-outline" size={16} color={colors[theme].icon} />
+
+          <ThemedText type="extraSmall">{duration.minutes}m</ThemedText>
+        </Flex>
+
+        <Flex direction="row" gap={2} align="center">
+          <FontAwesome6 name="dumbbell" size={14} color={colors[theme].icon} />
+
+          <ThemedText type="extraSmall">10,050 kg</ThemedText>
+        </Flex>
+
+        <Flex direction="row" gap={2} align="center">
+          <Ionicons
+            name="trophy-outline"
+            size={16}
+            color={colors[theme].icon}
+          />
+
+          <ThemedText type="extraSmall">2 PRs</ThemedText>
+        </Flex>
+      </Flex>
+
+      <VerticalSpacing size={6} />
+
+      {exercises?.map((exercise, index) => {
         return (
-          <>
-            <Flex justify="space-between" direction="row" gap={4}>
+          <View key={exercise.id}>
+            <Flex
+              justify="space-between"
+              align="flex-end"
+              direction="row"
+              gap={4}>
               <View style={{ flex: 1 }}>
                 <ThemedText type="small" key={exercise.id}>
                   {capitalize(exercise.name)}
@@ -53,7 +98,17 @@ export const PastWorkoutBox = ({ workout }: Props) => {
                 </ThemedText>
               </View>
 
-              <View style={{ flexShrink: 1 }}>
+              <Flex
+                direction="row"
+                gap={2}
+                align="center"
+                style={{ flexShrink: 1 }}>
+                <FontAwesome
+                  name="star-o"
+                  size={14}
+                  color={colors[theme].icon}
+                />
+
                 <ThemedText type="extraSmall">
                   {(() => {
                     const bestSet = exercise.sets?.reduce(
@@ -66,11 +121,19 @@ export const PastWorkoutBox = ({ workout }: Props) => {
                     return `${bestSet?.weight ?? 0} kg x ${bestSet?.reps ?? 0} reps`;
                   })()}{" "}
                 </ThemedText>
-              </View>
+              </Flex>
             </Flex>
 
-            <VerticalSpacing size={3} />
-          </>
+            {index !== exercises.length - 1 && (
+              <>
+                <VerticalSpacing size={3} />
+
+                <Divider />
+
+                <VerticalSpacing size={3} />
+              </>
+            )}
+          </View>
         );
       })}
     </View>
@@ -80,6 +143,6 @@ export const PastWorkoutBox = ({ workout }: Props) => {
 const styles = StyleSheet.create({
   container: {
     padding: spacing[4],
-    borderRadius: spacing[4],
+    borderRadius: 8,
   },
 });
