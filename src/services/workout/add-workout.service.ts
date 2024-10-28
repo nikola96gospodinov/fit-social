5c12f2f72683812b098477c7384917a6272f51ad
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-10-28 09:14:52 +0000

    feat: adding a workout now inserts data into multiple tables

--- src/services/workout/add-workout.service.ts
+++ src/services/workout/add-workout.service.ts
@@ -6,17 +6,43 @@ import { getOwnProfile } from "../profile/get-own-profile.service";
 const addWorkout = async () => {
   const profile = await getOwnProfile();
 
-  const { exercises, started } = useActiveWorkoutStore.getState();
+  const { exercises, started, sets } = useActiveWorkoutStore.getState();
 
-  const { data, error } = await supabase.from("workouts").insert({
-    started: started?.toISOString() ?? new Date().toISOString(),
-    exercises: exercises,
-    user_handle: String(profile.handle),
-  });
+  const { data, error } = await supabase
+    .from("workouts")
+    .insert({
+      started: started?.toISOString() ?? new Date().toISOString(),
+      user_handle: String(profile.handle),
+    })
+    .select()
+    .single();
 
   if (error) throw new Error(error.message);
 
-  return data;
+  const { data: workoutExercises, error: workoutExercisesError } =
+    await supabase
+      .from("workout_exercises")
+      .insert(
+        exercises.map((exercise) => ({ ...exercise, workout_id: data.id })),
+      )
+      .select();
+
+  if (workoutExercisesError) throw new Error(workoutExercisesError.message);
+
+  const { data: workoutSets, error: workoutSetsError } = await supabase
+    .from("exercise_sets")
+    .insert(
+      sets.map((set) => ({
+        ...set,
+        workout_exercise_id: workoutExercises.find(
+          (exercise) => exercise.exercise_id === set.workout_exercise_id,
+        )?.id,
+      })),
+    );
+
+  if (workoutSetsError) throw new Error(workoutSetsError.message);
+
+  return workoutSets;
 };
 
 export const useAddWorkout = () => {
