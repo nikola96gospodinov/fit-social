212493a3f6316c1184bfd75b6113bf9c9eb08e74
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-11-23 08:41:52 +0000

    feat: added the tab view package and a tab view to the follows page

--- src/screens/profile/follows/follows-content.component.tsx
+++ src/screens/profile/follows/follows-content.component.tsx
@@ -1,11 +1,41 @@
 import { useGetProfile } from "@/src/services/profile/get-profile.service";
 import { useNavigation } from "expo-router";
-import { View } from "react-native";
-import { useEffect } from "react";
+import { useWindowDimensions, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ThemedText } from "@/src/components/ui/themed-text/themed-text.component";
+import { TabView, SceneMap } from "react-native-tab-view";
+
+const FollowersTabs = () => {
+  return (
+    <View>
+      <ThemedText>Followers</ThemedText>
+    </View>
+  );
+};
+
+const FollowingTabs = () => {
+  return (
+    <View>
+      <ThemedText>Following</ThemedText>
+    </View>
+  );
+};
+
+const renderScene = SceneMap({
+  followers: FollowersTabs,
+  following: FollowingTabs,
+});
 
 export const FollowsContent = () => {
-  const navigation = useNavigation();
+  const layout = useWindowDimensions();
 
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "followers", title: "Followers" },
+    { key: "following", title: "Following" },
+  ]);
+
+  const navigation = useNavigation();
   const { data: profile } = useGetProfile();
 
   useEffect(() => {
@@ -16,5 +46,12 @@ export const FollowsContent = () => {
     }
   }, [profile?.handle, navigation]);
 
-  return <View></View>;
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
 };
