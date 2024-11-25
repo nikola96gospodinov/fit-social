68ed5a8af515d6bf923f01db32327c184f18eae6
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-11-25 08:57:59 +0000

    feat: styling the tab view to match app colors

--- src/screens/profile/follows/follows-content.component.tsx
+++ src/screens/profile/follows/follows-content.component.tsx
@@ -1,9 +1,15 @@
 import { useGetProfile } from "@/src/services/profile/get-profile.service";
 import { useNavigation } from "expo-router";
-import { useWindowDimensions, View } from "react-native";
+import { useColorScheme, useWindowDimensions, View } from "react-native";
 import { useEffect, useState } from "react";
 import { ThemedText } from "@/src/components/ui/themed-text/themed-text.component";
-import { TabView, SceneMap } from "react-native-tab-view";
+import { TabView, SceneMap, TabBarProps, TabBar } from "react-native-tab-view";
+import { colors } from "@/src/constants/colors.constants";
+
+const defaultRoutes = [
+  { key: "followers", title: "Followers" },
+  { key: "following", title: "Following" },
+];
 
 const FollowersTabs = () => {
   return (
@@ -21,6 +27,22 @@ const FollowingTabs = () => {
   );
 };
 
+const FollowingTabBar = (
+  props: TabBarProps<(typeof defaultRoutes)[number]>,
+) => {
+  const theme = useColorScheme() ?? "light";
+
+  return (
+    <TabBar
+      {...props}
+      style={{ backgroundColor: colors[theme].cardBackground }}
+      activeColor={colors[theme].text}
+      inactiveColor={colors[theme].supportingText}
+      indicatorStyle={{ backgroundColor: colors[theme].tintBackground }}
+    />
+  );
+};
+
 const renderScene = SceneMap({
   followers: FollowersTabs,
   following: FollowingTabs,
@@ -30,10 +52,7 @@ export const FollowsContent = () => {
   const layout = useWindowDimensions();
 
   const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: "followers", title: "Followers" },
-    { key: "following", title: "Following" },
-  ]);
+  const [routes] = useState(defaultRoutes);
 
   const navigation = useNavigation();
   const { data: profile } = useGetProfile();
@@ -52,6 +71,7 @@ export const FollowsContent = () => {
       renderScene={renderScene}
       onIndexChange={setIndex}
       initialLayout={{ width: layout.width }}
+      renderTabBar={FollowingTabBar}
     />
   );
 };
